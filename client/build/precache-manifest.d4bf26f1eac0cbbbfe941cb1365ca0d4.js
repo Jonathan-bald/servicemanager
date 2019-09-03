ad31f27038f3d1f9d10ecfbe86d46b4105ca4ea6
baldyj117 <baldyj117@yahoo.com>
2019-09-03 13:43:14 -0500

    prod

---
+++ client/build/precache-manifest.d4bf26f1eac0cbbbfe941cb1365ca0d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f79a5579c5ba9ef3a853a1877d4ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949563efbfc2b71e05ff",
+    "url": "/static/css/main.3647f389.chunk.css"
+  },
+  {
+    "revision": "6c2515bc1e529fefc244",
+    "url": "/static/js/2.da0cbe04.chunk.js"
+  },
+  {
+    "revision": "949563efbfc2b71e05ff",
+    "url": "/static/js/main.df4d4190.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
